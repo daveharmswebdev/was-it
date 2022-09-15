a7d756b5707379dbd2da2cabb9bcc8ca24e1da26
Dave Harms <daveharmswebdev@gmail.com>
2022-09-15 08:00:00 -0500

    added paging

--- apps/demo/src/app/people/people-table/people-table.component.ts
+++ apps/demo/src/app/people/people-table/people-table.component.ts
@@ -1,16 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { IUser } from '../../models/IUser';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'was-it-people-table',
   templateUrl: './people-table.component.html',
   styleUrls: ['./people-table.component.css'],
 })
-export class PeopleTableComponent implements OnInit {
-  @Input() people: IUser[] = [];
+export class PeopleTableComponent {
   displayedColumns = ['id', 'email', 'zipCode'];
+  private _dataSource!: MatTableDataSource<IUser>;
 
-  constructor() {}
+  get dataSource() {
+    return this._dataSource;
+  }
 
-  ngOnInit(): void {}
+  @Input()
+  set people(value: IUser[]) {
+    if (value) {
+      this._dataSource = new MatTableDataSource<IUser>(value);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 }
